8e1b4f2d5121ecf7728df1f98bd6fe1aff879473
ZoePlayoust <100842308+ZoePlayoust@users.noreply.github.com>
2023-04-13 15:18:02 +0200

    cleaned dependencies

--- src/components/subredditList.js
+++ src/components/subredditList.js
@@ -1,4 +1,4 @@
-import React from "react";
+
 
 // implement a .map function to iterate through the subreddit object to display all subreddits
 const subredditList = ['Succulents', 'Orchids', 'Plants','Gardening', 'Begonias']; 
